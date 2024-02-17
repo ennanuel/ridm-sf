0d18e27f62efbbb364e26fa4caa86a37804f3f87
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-17 18:50:05 +0100

    made changes

--- netlify/functions/scoreplug-match-update/index.mjs
+++ netlify/functions/scoreplug-match-update/index.mjs
@@ -3,7 +3,6 @@ import axios from "axios";
 const webhookHandler = async (event, context) => {
     try {
         console.log(event.body);
-        console.log(event);
         // const { time_to_run } = JSON.parse(event.body);
         // console.log(time_to_run);
         return new Response(null, { status: 204 });
