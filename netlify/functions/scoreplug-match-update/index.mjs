ff6cdd3744ee63118ed9a46eddb67e09311e4578
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-17 18:25:25 +0100

    Added function to handle scoreplug match update

---
+++ netlify/functions/scoreplug-match-update/index.mjs
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const webhookHandler = async (event, context) => {
+    try {
+        const { time_to_run } = JSON.parse(event.body);
+        console.log(time_to_run);
+        return new Response(null, { status: 204 });
+    } catch (error) {
+        console.error(error);
+        return new Response(error.message, { status: 500 });
+    }
+};
+
+export default webhookHandler;
+
+export const config = {
+    path: "/match-update"
+};
