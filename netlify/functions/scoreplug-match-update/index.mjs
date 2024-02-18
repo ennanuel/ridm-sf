fa88d62fa26f832ceb8e8bc76ef433fead61eb56
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-18 16:31:34 +0100

    Refactored code

--- netlify/functions/scoreplug-match-update/index.mjs
+++ netlify/functions/scoreplug-match-update/index.mjs
@@ -1,12 +1,31 @@
 import axios from "axios";
 
+const cancelToken = axios.CancelToken;
+const source = cancelToken.source();
+
+function getTimesToMakeCall(time, numberOfSecondsInMilliSeconds = 5000) {
+    const timetoEndCall = (new Date(time)).getTime();
+    const currentTime = Date.now();
+    if (currentTime <= timetoEndCall) return 0;
+    const timeRemainingInMilliseconds = timetoEndCall - currentTime;
+    const timesToMakeCall = Math.ceil(timeRemainingInMilliseconds / numberOfSecondsInMilliSeconds);
+    return timesToMakeCall;
+}
+
 const webhookHandler = async (event, context) => {
     try {
-        const request = await event.json();
-        console.log(request);
-        // const { time_to_run } = JSON.parse(event.body);
-        // console.log(time_to_run);
-        return new Response(null, { status: 204 });
+        const requestBody = await event.json();
+        const { timeToRun, timesCalled } = requestBody;
+        let timesToMakeCall = 0;
+
+        if (!timesCalled && !timeToRun) return new Response({ message: "nothing to do" }, { status: 200 });
+        else if (!timeToRun && timesCalled) timesToMakeCall = getTimesToMakeCall(timeToRun);
+        else timesToMakeCall--;
+
+        axios.get('/match-update');
+        source.cancel("Request cancelled");
+
+        return new Response({ message: `called again: ${timesToMakeCall} times`}, { status: 204 });
     } catch (error) {
         console.error(error);
         return new Response(error.message, { status: 500 });
