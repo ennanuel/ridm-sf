7983ed01f12842d58a004365981c542b2dca10d4
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-18 16:35:58 +0100

    something...

--- netlify/functions/scoreplug-match-update/index.mjs
+++ netlify/functions/scoreplug-match-update/index.mjs
@@ -17,15 +17,17 @@ const webhookHandler = async (event, context) => {
         const requestBody = await event.json();
         const { timeToRun, timesCalled } = requestBody;
         let timesToMakeCall = 0;
+        console.log()
 
         if (!timesCalled && !timeToRun) return new Response({ message: "nothing to do" }, { status: 200 });
-        else if (!timeToRun && timesCalled) timesToMakeCall = getTimesToMakeCall(timeToRun);
-        else timesToMakeCall--;
+        else if (timeToRun && !timesCalled) timesToMakeCall = getTimesToMakeCall(timeToRun);
+        else timesToMakeCall = timesCalled;
 
-        axios.get('/match-update');
+        timesToMakeCall--
+        axios.post('/match-update', { timeToRun, timesCalled: timesToMakeCall });
         source.cancel("Request cancelled");
 
-        return new Response({ message: `called again: ${timesToMakeCall} times`}, { status: 204 });
+        return new Response({ message: `called again: ${timesToMakeCall} times`}, { status: 200 });
     } catch (error) {
         console.error(error);
         return new Response(error.message, { status: 500 });
