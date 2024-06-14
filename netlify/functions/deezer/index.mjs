03a5b6ec57e35054856bba73de01fe8ed979bf2d
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-06-14 11:35:26 +0100

    ...

--- netlify/functions/deezer/index.mjs
+++ netlify/functions/deezer/index.mjs
@@ -5,23 +5,15 @@ async function deezerHandler(req) {
         const url = req.url;
         const queryString = url.replace(/(https|http):\/\/(\w|.)+app\/api\/deezer(\/)*/, '').split(/(\?|\&)/).map(query => query.split('='));
         const queries = queryString.map(([key, entry]) => ({ [key]: entry })).reduce((entries, entry) => ({ ...entries, ...entry }), {});
-        return {
-            body: JSON.stringify(queries),
-            statusCode: 200
-        };
 
-        const { path } = req.query;
-
-        const URL = `${process.env.DEEZER_URL}${path}`;
+        const { path, ...params } = queries;
+        const URL = `${process.env.DEEZER_URL}/${path}`;
 
         const response = await axios.get(URL, { params });
 
         console.log("API call successful!");
         
-        return {
-            body: response.data,
-            statusCode: 200
-        }
+        return new Response(JSON.stringify(response.data), 200);
     } catch (error) {
         console.error(error);
         return new Response(error.message, { status: 500 });
