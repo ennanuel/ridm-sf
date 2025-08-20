12a8e4393d6bfd20b013af5eb3a84499976391e4
Emmanuel Ezema <emmanuelezema6@gmail.com>
2025-08-20 11:53:57 +0100

    Refactored code

--- utils/cancel.js
+++ utils/cancel.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const cancelToken = axios.CancelToken;
-const source = cancelToken.source();
-
 const cancelAfterFiveSeconds = () => setTimeout(() => {
+    const cancelToken = axios.CancelToken;
+    const source = cancelToken.source();
+
     source.cancel("Time out!");
-});
+}, 5000);
 
 export {
     cancelAfterFiveSeconds
-}
+};
