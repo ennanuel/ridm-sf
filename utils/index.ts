5bf6fbef6e3a53aeceedc95f790a7b9a685d528d
Emmanuel Ezema <emmanuelezema6@gmail.com>
2025-08-21 22:26:10 +0100

    imported and exported 'getDeezerSong' function

--- utils/index.ts
+++ utils/index.ts
@@ -2,12 +2,14 @@
 
 import cancelAfterFiveSeconds from "./cancel";
 import getLyrics from "./getLyrics";
+import getDeezerSong from "./getSong";
 import { getParamsOutOfUrl, generateGeniusApiOption } from "./url";
 
 
 export {
     cancelAfterFiveSeconds,
     getLyrics,
+    getDeezerSong,
     generateGeniusApiOption,
     getParamsOutOfUrl,
 };
