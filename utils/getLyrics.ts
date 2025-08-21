5160c79c6cee7a514f4f158bf79f3df874c75836
Emmanuel Ezema <emmanuelezema6@gmail.com>
2025-08-21 22:25:49 +0100

    Modified function to get lyrics from lyrics.ovh

--- utils/getLyrics.ts
+++ utils/getLyrics.ts
@@ -1,25 +1,14 @@
 
 import axios from "axios";
-import { load as cheerioLoad } from "cheerio";
 
-
-const BASE_URL = 'https://www.azlyrics.com';
-const SEARCH_TEXT = 'Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that.';
-const HEADERS = {
-	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
-};
-
+const BASE_URL = 'https://api.lyrics.ovh/v1';
 
 export default async function getLyrics2 (songTitle?: string, artistName?: string) {
 	try {
         if(songTitle && artistName) {
-            const artist = removeNonAlphabets(artistName);
-            const song = removeNonAlphabets(songTitle);
-            const url = `${BASE_URL}/lyrics/${artist}/${song}.html`;
-
-            const { data } = await axios.get(url, { headers: HEADERS });
-            const lyrDat = selectElementByCommentContent(data, SEARCH_TEXT);
-            return lyrDat;
+			const url = `${BASE_URL}/${artistName}/${songTitle}`;
+            const { data } = await axios.get(url);
+            return data;
         } else {
             throw new Error('Song has to have a title and artist name')
         }
@@ -27,46 +16,3 @@ export default async function getLyrics2 (songTitle?: string, artistName?: strin
 		throw e;
 	}
 };
-
-
-function removeNonAlphabets(value: string): string {
-    return value
-		.toLowerCase()
-        .replace(/[^a-z]/ig, '')
-        .trim();
-}
-
-function selectElementByCommentContent(content: any, commentContent: string): string {
-	let lyrics = "";
-
-	const $ = cheerioLoad(content);
-
-	const res: string[] = [];
-
-	$('div').each((_, el) => {
-		const strEl = $(el).html();
-		res.push(strEl || '');
-	});
-
-	console.log(res);
-	
-	return res.join(' ');
-
-	$('div').each((_, element) => {
-		$(element).contents().each((_, node) => {
-			if (node.type === 'comment' && node.data.includes(commentContent)) {
-				const elementHTML = $(element).html();
-                if(elementHTML) {
-					let snippet = elementHTML
-						.replace(/<br>/g, '\n')
-                        .replace(/(^" *| "$)/g, '')
-						.replace(/<(?!\s*br\s*\/?)[^>]+>/gi, '');
-					lyrics += snippet;
-                }
-				return false;
-			}
-		});
-		if (lyrics) return false;
-	});
-	return lyrics;
-}
