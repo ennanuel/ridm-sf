7df0ff9a21e8082b8dcbd3633f36881259516c48
Emmanuel Ezema <emmanuelezema6@gmail.com>
2025-08-21 10:57:40 +0100

    Exported all functions within utils folder

---
+++ utils/index.ts
@@ -0,0 +1,15 @@
+
+
+import cancelAfterFiveSeconds from "./cancel";
+import getLyrics from "./genius-lyrics-api/getLyrics";
+import getSong from "./genius-lyrics-api/getSong";
+import { getParamsOutOfUrl, generateGeniusApiOption } from "./url";
+
+
+export {
+    cancelAfterFiveSeconds,
+    getLyrics,
+    generateGeniusApiOption,
+    getParamsOutOfUrl,
+    getSong
+};
