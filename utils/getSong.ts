c9d0ffab5b92a94114452c8639a325febf3ee71e
Emmanuel Ezema <emmanuelezema6@gmail.com>
2025-08-22 15:21:37 +0100

    Added logic to handle when song data isn't found

--- utils/getSong.ts
+++ utils/getSong.ts
@@ -5,6 +5,7 @@ export default async function getDeezerSong(url: string) {
     try {
         const queries = getParamsOutOfUrl(url);
         const { data } = await axios.get(`${process.env.DEEZER_URL}/track/${queries?.songId}`);
+        if (!data) throw { message: "Could not find song", statusCode: 404 };
         return data;
     } catch (error) {
         throw error;
